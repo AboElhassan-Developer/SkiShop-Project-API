beaabb35b5c980dfb0496f8b44a32804f08e3d39
AboElhassan-Developer <mamdohbassem2@gmail.com>
2025-12-14 14:22:21 +0200

    Initial commit - SkiShop project setup
    part3

--- SkiShop.API/Program.cs
+++ SkiShop.API/Program.cs
@@ -1,12 +1,14 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace SkiShop.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,7 @@ namespace SkiShop.API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -39,6 +42,21 @@ namespace SkiShop.API
 
             app.MapControllers();
 
+            try
+            {
+                using var scope = app.Services.CreateScope();
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<StoreContext>();
+                await context.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+
+            }
             app.Run();
         }
     }
